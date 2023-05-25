582d25d6b7187dd6cbfff83c1219d80a49c5f488
paul_ng_hkg <paul_ng_hkg@Ensigns-MacBook-Air.local>
2023-05-25 18:40:29 +0800

    Adding comment to file'

--- features/user/userSlice.js
+++ features/user/userSlice.js
@@ -2,16 +2,21 @@ const createSlice = require('@reduxjs/toolkit').createSlice
 const createAsyncThunk = require('@reduxjs/toolkit').createAsyncThunk
 const axios = require('axios')
 
+//this is the initialState of the slice
 const initialState = {
     loading: false,
     users: [],
     error: ''
 }
 
+//this is using the createAsyncThunk to perform the async function and a callback funciton to return a promise
 const fetchUsers = createAsyncThunk('user/fetchUser',() => {
     return axios.get("https://jsonplaceholder.typicode.com/users")
     .then(response => response.data.map((user) => user.id))
 })
+
+//inside the extraReducers, using builder to addCase and getting the promise of fetchUsers, pending, fulfilled and rejected are the status of the promise
+//getting the result and update the state
 
 const userSlice = createSlice({
     name: 'user',
@@ -33,5 +38,15 @@ const userSlice = createSlice({
     }
 })
 
+//export the reducer
+//export the fetchUsers function and will used in the index.js
+
+//inside the store, will add the reducer
+//const userReducer = required('userSlice')
+//user: userReducers
+
+//inside index.js
+//since there is no reducer, we will only export the function
+//const fetchUsers = required('userSlice').fetchUsers
 module.exports = userSlice.reducer
 module.exports.fetchUsers = fetchUsers
